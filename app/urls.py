d255c2e227b7a8a7c09847de26956c42896cb0d5
Ahmed-Elatar <a7med.elatar55@gmail.com>
2024-05-09 17:32:45 +0300

    ----

--- app/urls.py
+++ app/urls.py
@@ -4,5 +4,8 @@ from .views import *
 urlpatterns = [
     
     path('', index ,name="index"),
+    path('login/',user_login,name="login"),
+    path('logout/',user_logout,name="logout"),
+    path('signup/',user_signup,name="signup"),
 
 ]
--- app/views.py
+++ app/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render,redirect,get_object_or_404
 from django.http import HttpResponse,JsonResponse
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from django.contrib.auth import authenticate,login,logout,get_user
+from django.contrib.auth.models import User, Group
 from .forms import *
 from .models import *
 from .serializers import *
@@ -14,4 +16,65 @@ from .serializers import *
 
 
 def index(request):
+
+    # if not request.user.is_authenticated :
+    #     return redirect('login')
+
+    
+    print(request.user.is_authenticated)
     return HttpResponse("HOME................")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+####################################################################################################
+##################                  Authentication
+
+#sign-up 
+def user_signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
+
+
+
+# login
+def user_login(request):
+    if request.user.is_authenticated:
+        return redirect ('logout')
+    if request.method == 'POST':
+        
+
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user:
+                login(request, user)    
+                return redirect('index')
+    else:
+        form = LoginForm()
+    return render(request, 'login.html', {'form': form})
+
+
+# logout page
+def user_logout(request):
+    logout(request)
+    return redirect('login')
